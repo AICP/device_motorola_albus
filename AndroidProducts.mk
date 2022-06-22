LineageOS/android_device_motorola_albus

albus: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627 | AOSP
Test: lunch  [see entries in list]

Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_albus.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_albus-user \
+    lineage_albus-userdebug \
+    lineage_albus-eng

AICP/device_motorola_albus

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aicp_albus.mk
+
+COMMON_LUNCH_CHOICES := \
+    aicp_albus-user \
+    aicp_albus-userdebug \
+    aicp_albus-eng